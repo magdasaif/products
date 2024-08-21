283c08583a49d7f35bca34bc6c8b26626d0a2480
magda <magdasaif3@gmail.com>
2024-08-21 15:18:16 +0300

    publish view file

--- src/app/providers/ProductServiceProvider.php
+++ src/app/providers/ProductServiceProvider.php
@@ -9,15 +9,22 @@ class ProductServiceProvider extends ServiceProvider
         //==============================================================================================
         //to load package routes in any project background
         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+        //==============================================================================================
+        //to load package views in any project background
         $this->loadViewsFrom(__DIR__.'/../../resources/views','products');
-
+        //==============================================================================================
         //to load package migrations files in any project background
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-
+        //==============================================================================================
         //to handle publish step for migrations file 
         $this->publishes([
             __DIR__.'/../../database/migrations' => database_path('migrations')
         ], 'product-migrations');
+        //==============================================================================================
+        //to handle publish step for views file 
+        $this->publishes([
+            __DIR__.'/../../resources/views' => base_path('resources/views')
+        ], 'product-views');
 
         //==============================================================================================
         //try to publish all package folder
